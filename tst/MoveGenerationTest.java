f10d68b318190ab3191008833bbc3461c454f988
bkthomps <bailey.kyle.thompson@gmail.com>
2022-04-20 02:35:37 -0400

    Add maven and junit

---
+++ tst/MoveGenerationTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MoveGenerationTest {
+    @Test
+    void addition() {
+        assertEquals(2, 1 + 1);
+    }
+}
