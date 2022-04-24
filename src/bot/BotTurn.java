2950def93c74c3794bffa319e246dfc8f52bac9c
bkthomps <bailey.kyle.thompson@gmail.com>
2022-04-23 21:52:24 -0400

    Add piece values

--- src/bot/BotTurn.java
+++ src/bot/BotTurn.java
@@ -1,9 +1,23 @@
 package bot;
 
 import backend.Game;
+import backend.Piece;
 import backend.State;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public final class BotTurn {
+    private static final Map<Piece.Type, Integer> values = new HashMap<>();
+
+    static {
+        values.put(Piece.Type.PAWN, 100);
+        values.put(Piece.Type.KNIGHT, 300);
+        values.put(Piece.Type.BISHOP, 300);
+        values.put(Piece.Type.ROOK, 500);
+        values.put(Piece.Type.QUEEN, 900);
+    }
+
     public static State perform(Game game) {
         var state = game.generateMoves();
         if (state.isTerminal()) {
